cc1dcb64ca34602a6cf0be20e938f877fb657e80
George Hester <48884343+georgehester@users.noreply.github.com>
2025-09-16 18:21:48 +0100

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,31 @@ const jetBrainsMono = JetBrains_Mono({
 export const metadata: Metadata = {
     title: "LeedsHack | Leeds Computing Society",
     keywords: ["LeedsHack", "LeedsHack 2026", "LeedsHack 2025", "Leeds Computing Society", "University of Leeds", "School of Computing", "LUU", "Leeds University Union", "LUUCompSoc", "CompSoc", "Computing Society"],
-    description: "The official student computing society of the University of Leeds. We work with the School of Computing and local companies to run a range of fun events and socials, providing plenty of activities for everyone.",
+    description: "LeedsHack is the official hackathon of the Leeds Computing Society. LeedsHack is supported by the University of Leeds, School of Computing and our sponsors. We invite students from Universities across the UK to participate in a free 24-hour hackathon.",
+    icons: {
+        icon: [
+            { url: "/favicon.ico", sizes: "48x48" },
+            { url: "/favicon.svg", sizes: "any", type: "image/svg+xml" },
+            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+            { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+            { url: "/favicon-128x128.png", sizes: "128x128", type: "image/png" },
+            { url: "/favicon-196x196.png", sizes: "196x196", type: "image/png" }
+        ],
+        apple: [
+            { url: "/apple-touch-icon-57x57.png", sizes: "57x57", type: "image/png", rel: "apple-touch-icon-precomposed" },
+            { url: "/apple-touch-icon-60x60.png", sizes: "60x60", type: "image/png", rel: "apple-touch-icon-precomposed" },
+            { url: "/apple-touch-icon-72x72.png", sizes: "72x72", type: "image/png", rel: "apple-touch-icon-precomposed" },
+            { url: "/apple-touch-icon-76x76.png", sizes: "76x76", type: "image/png", rel: "apple-touch-icon-precomposed" },
+            { url: "/apple-touch-icon-114x114.png", sizes: "114x114", type: "image/png", rel: "apple-touch-icon-precomposed" },
+            { url: "/apple-touch-icon-120x120.png", sizes: "120x120", type: "image/png", rel: "apple-touch-icon-precomposed" },
+            { url: "/apple-touch-icon-144x144.png", sizes: "144x144", type: "image/png", rel: "apple-touch-icon-precomposed" },
+            { url: "/apple-touch-icon-152x152.png", sizes: "152x152", type: "image/png", rel: "apple-touch-icon-precomposed" }
+        ]
+    },
     twitter: {
         title: "LeedsHack | Leeds Computing Society | Leeds University Union",
-        description: "The official student computing society of the University of Leeds. We work with the School of Computing and local companies to run a range of fun events and socials, providing plenty of activities for everyone."
+        description: "LeedsHack is the official hackathon of the Leeds Computing Society. LeedsHack is supported by the University of Leeds, School of Computing and our sponsors. We invite students from Universities across the UK to participate in a free 24-hour hackathon."
     }
 };
 
