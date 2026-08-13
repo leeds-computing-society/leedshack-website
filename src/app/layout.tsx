a638c4206da06f13d49d95056639824a0a715b25
Omar Abdelgawad <sdgt0809@leeds.ac.uk>
2026-08-13 13:39:25 +0300

    Added Past Events dropdown to header, added LeedsHack 2027 logo, fixed mobile menu height, update page title to 2027

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,45 +11,53 @@ import { defaultMetadata } from "./metadata";
 import { ReactScan } from "@/app/components/react-scan";
 
 const jetBrainsMono = JetBrains_Mono({
-    subsets: ["latin"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    ...defaultMetadata,
-    title: "LeedsHack 2026 | LeedsHack"
+  ...defaultMetadata,
+  title: "LeedsHack 2027 | LeedsHack",
 };
 
 export const viewport: Viewport = {
-    themeColor: "#00074d"
+  themeColor: "#00074d",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
-}>)
-{
-    return (
-        <html lang="en">
-            <ReactScan></ReactScan>
-            <head>
-            </head>
-            <body className={jetBrainsMono.className + " absolute h-full w-full m-0 p-0 bg-black"}>
-                <div className="absolute w-full flex flex-col overflow-hidden">
-                    <Background></Background>
-                    <MouseHighlight></MouseHighlight>
-                    <Header></Header>
-                    <a className="block w-12 min-[48rem]:w-24 fixed right-6 min-[48rem]:right-12 top-0 z-50" href="https://mlh.io/eu?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=black" target="_blank">
-                        <img className="w-full" src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg" alt="Major League Hacking 2026 Hackathon Season"></img>
-                    </a>
-                    <div className="min-h-[100vh] flex flex-col">
-                        <div className="pt-14 grow flex flex-col">
-                            {children}
-                        </div>
-                    </div>
-                    <Footer></Footer>
-                </div>
-            </body>
-        </html>
-    );
-};
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ReactScan></ReactScan>
+      <head></head>
+      <body
+        className={
+          jetBrainsMono.className + " absolute h-full w-full m-0 p-0 bg-black"
+        }
+      >
+        <div className="absolute w-full flex flex-col overflow-hidden">
+          <Background></Background>
+          <MouseHighlight></MouseHighlight>
+          <Header></Header>
+          <a
+            className="block w-12 min-[48rem]:w-24 fixed right-6 min-[48rem]:right-12 top-0 z-50"
+            href="https://mlh.io/eu?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=black"
+            target="_blank"
+          >
+            <img
+              className="w-full"
+              src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
+              alt="Major League Hacking 2026 Hackathon Season"
+            ></img>
+          </a>
+          <div className="min-h-[100vh] flex flex-col">
+            <div className="pt-14 grow flex flex-col">{children}</div>
+          </div>
+          <Footer></Footer>
+        </div>
+      </body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 export default function Page() {
   return (
     <div className="flex flex-row justify-center items-center grow text-white">
       <div className="flex flex-col text-center mx-6 min-[64rem]:mx-12 min-[80rem]:mx-48 grow max-w-[96rem]">
+        <Image
+          className="max-w-[24rem] w-full mx-auto"
+          alt="LeedsHack 2027 Logo"
+          src="/leeds-hack/2027-logo.png"
+          width={1511}
+          height={925}
+        />
         <h1 className="text-header-lg font-bold">COMING SOON!!</h1>
         <h6 className="text-header-md font-bold mt-6">Stay tuned.</h6>
       </div>
